eaf8dc62a99c2feb09f03ac4ee974482be4c7b12
rgoshen <rick.goshen@gmail.com>
2021-04-14 13:23:01 -0700

    added get all companies

--- routes/companies.js
+++ routes/companies.js
@@ -1,1 +1,29 @@
-// comapanies.js
+/** Routes for companies. */
+
+const express = require("express");
+const ExpressError = require("../expressError");
+const db = require("../db");
+
+let router = new express.Router();
+
+/** GET / => list of companies.
+ *
+ * =>  {companies: [{code, name, descrip}, {code, name, descrip}, ...]}
+ *
+ * */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const result = await db.query(
+      `SELECT code, name
+           FROM companies
+           ORDER BY name`
+    );
+
+    return res.json({ companies: result.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
