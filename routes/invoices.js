dfdfc1c8cf89a9e641817c10ec601dd0273016ba
rgoshen <rick.goshen@gmail.com>
2021-04-14 14:12:08 -0700

    added get specific inv detail data

--- routes/invoices.js
+++ routes/invoices.js
@@ -26,4 +26,58 @@ router.get("/", async function (req, res, next) {
   }
 });
 
+/** GET /[id] => detail on invoice
+ *
+ * =>  {invoices: {id,
+ *                amt,
+ *                paid,
+ *                add_date,
+ *                paid_date,
+ *                company: {code, name, description}}}
+ *
+ * */
+
+router.get("/:id", async function (req, res, next) {
+  try {
+    let id = req.params.id;
+
+    const result = await db.query(
+      `SELECT i.id, 
+                  i.comp_code, 
+                  i.amt, 
+                  i.paid, 
+                  i.add_date, 
+                  i.paid_date, 
+                  c.name, 
+                  c.description 
+           FROM invoices AS i
+             INNER JOIN companies AS c ON (i.comp_code = c.code)  
+           WHERE id = $1`,
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      throw new ExpressError(`No such invoice: ${id}`, 404);
+    }
+
+    const data = result.rows[0];
+    const invoice = {
+      id: data.id,
+      company: {
+        code: data.comp_code,
+        name: data.name,
+        description: data.description,
+      },
+      amt: data.amt,
+      paid: data.paid,
+      add_date: data.add_date,
+      paid_date: data.paid_date,
+    };
+
+    return res.json({ invoice: invoice });
+  } catch (err) {
+    return next(err);
+  }
+});
+
 module.exports = router;
