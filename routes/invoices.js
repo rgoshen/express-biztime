2c4c9459b9f04cde06780c3bbd17ca953da78366
rgoshen <rick.goshen@gmail.com>
2021-04-14 14:02:32 -0700

    added get all invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,1 +1,29 @@
-//invoices.js
+/** Routes for invoices. */
+
+const express = require("express");
+const ExpressError = require("../expressError");
+const db = require("../db");
+
+let router = new express.Router();
+
+/** GET / => list of invoices.
+ *
+ * =>  {invoices: [{id, comp_code}, ...]}
+ *
+ * */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const result = await db.query(
+      `SELECT id, comp_code
+           FROM invoices 
+           ORDER BY id`
+    );
+
+    return res.json({ invoices: result.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
