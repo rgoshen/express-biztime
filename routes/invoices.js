e103bef75cddce3f824ba260c1d83e81aaec8efa
rgoshen <rick.goshen@gmail.com>
2021-04-14 14:16:50 -0700

    added update specific invoice

--- routes/invoices.js
+++ routes/invoices.js
@@ -103,4 +103,52 @@ router.post("/", async function (req, res, next) {
   }
 });
 
+/** PUT /[code] => update invoice
+ *
+ * {amt, paid}  =>  {id, comp_code, amt, paid, add_date, paid_date}
+ *
+ * If paying unpaid invoice, set paid_date; if marking as unpaid, clear paid_date.
+ * */
+
+router.put("/:id", async function (req, res, next) {
+  try {
+    let { amt, paid } = req.body;
+    let id = req.params.id;
+    let paidDate = null;
+
+    const currResult = await db.query(
+      `SELECT paid
+           FROM invoices
+           WHERE id = $1`,
+      [id]
+    );
+
+    if (currResult.rows.length === 0) {
+      throw new ExpressError(`No such invoice: ${id}`, 404);
+    }
+
+    const currPaidDate = currResult.rows[0].paid_date;
+
+    if (!currPaidDate && paid) {
+      paidDate = new Date();
+    } else if (!paid) {
+      paidDate = null;
+    } else {
+      paidDate = currPaidDate;
+    }
+
+    const result = await db.query(
+      `UPDATE invoices
+           SET amt=$1, paid=$2, paid_date=$3
+           WHERE id=$4
+           RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [amt, paid, paidDate, id]
+    );
+
+    return res.json({ invoice: result.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+});
+
 module.exports = router;
